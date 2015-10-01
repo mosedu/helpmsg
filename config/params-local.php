2754dc77d4dd67f4ef08739ac74d433e2db6197c
Victor <devmosedu@yandex.ru>
2015-10-01 13:30:20 +0300

    Initial commit

---
+++ config/params-local.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KozminVA
+ * Date: 01.10.2015
+ * Time: 11:38
+ */
+
+return [
+    'user.groups' => [
+        'user' => [2, 3, 4, 5, 6],
+        'admin' => [7, 8, ],
+    ]
+];
---
+++ config/web-local.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KozminVA
+ * Date: 01.10.2015
+ * Time: 11:51
+ */
+
+$config = [
+    'components' => [
+        /*
+                'assetManager' => [
+                    'bundles' => [
+                        'yii\bootstrap\BootstrapAsset' => [
+                            'sourcePath' => null,
+                            'basePath' => '@webroot',
+                            'baseUrl' => '@web',
+                            'css' => ['css/custom-bootstrap.css'],
+                        ],
+                    ],
+                ],
+        */
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning', 'info', ], // 'trace'
+                    'logFile' => '@app/runtime/logs/web.log',
+                    'maxFileSize' => 250,
+                    'maxLogFiles' => 2,
+                ],
+            ],
+        ],
+    ],
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
